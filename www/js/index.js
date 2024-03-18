caa5152735818ed377970d4b54de4de2d3fe9a1e
Jacob Janak <jjanak@paytronix.com>
2024-03-18 11:19:51 -0400

    Init

---
+++ www/js/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    window.location = 'https://mrpickles.orderexperience.net/61a8e4a195b701075d8b457c/menu';
+}
